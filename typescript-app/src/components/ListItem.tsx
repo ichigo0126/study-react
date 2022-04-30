9dce7df9053398a39ff619f78f737a6a3807027d
shoma <luckyshoma@gmail.com>
2022-04-30 09:28:54 +0900

    type-definition

---
+++ typescript-app/src/components/ListItem.tsx
@@ -0,0 +1,15 @@
+import type { User } from "../types/user";
+import type { FC } from "react";
+
+export const ListItem: FC<User> = props => {
+    const {id, name, age, personalColor, hobbies } = props;
+    return (
+        <p style={{ color: personalColor }}>
+            {id}: {name}({age}) {hobbies?.join(" / ")}
+        </p>
+    );
+};
+
+ListItem.defaultProps = {
+    personalColor: "gray"
+};
